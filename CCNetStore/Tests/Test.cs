cc74818812daf4d177fa256a2993771f84634f88
vlarikev <vlad-zhukov-1999@mail.ru>
2019-12-26 13:43:32 +0300

    Testing Added; fixed code;

---
+++ CCNetStore/Tests/Test.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using CCNetStore.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CCNetStore.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        private CCStoreEntities db = new CCStoreEntities();
+        [Test]
+        public void TestCartToList()
+        {
+            List<cart> l = db.carts.Where(c => c.clientId == 3).ToList();
+            Assert.AreEqual(4, l.Count());
+        }
+        [Test]
+        public void TestCartSum()
+        {
+            var carts = db.carts.Include(c => c.client).Include(c => c.product);
+
+            decimal i = carts.Where(c => c.client.clientLogin == "client@mail.com").Sum(p => p.product.productPrice).Value;
+            Assert.AreEqual(38000.00, i);
+        }
+    }
+}
